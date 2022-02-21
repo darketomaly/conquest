5a1f78be85631b87c40c016b54edd75729d2ef13
darketomaly <darketomaly@gmail.com>
2022-02-21 01:36:41 -0600

    added signal to track, removed coroutine

--- Assets/Project Assets/Behaviors/Cinematic Intro/CinematicIntroManager.cs
+++ Assets/Project Assets/Behaviors/Cinematic Intro/CinematicIntroManager.cs
@@ -1,27 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class CinematicIntroManager : MonoBehaviour {
 
     AsyncOperation op;
 
-    [SerializeField] PlayableDirector director;
-
     void Start() {
 
-        //placeholder test
         op = SceneManager.LoadSceneAsync("Title Screen");
         op.allowSceneActivation = false;
-
-        StartCoroutine(Test());
     }
 
-    IEnumerator Test() {
+    public void ActivateTitleScreen(){
 
-        yield return new WaitForSeconds((float)director.duration);
         op.allowSceneActivation = true;
+    }
+
+    public void PrintMessage(bool value) {
+
+        Debug.Log($"<color=olive>Printing {value}</color>");
     }
 }
